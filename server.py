6432cb1848bde5a1293fa5868fd48041e34b75d3
limyeonsang <limquo@gmail.com>
2021-06-21 23:56:38 +0900

    modify

--- server.py
+++ server.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 # slack_web_client = WebClient(token = os.environ.get("SLACKBOT_TOKEN"))
 
-@app.route("/") 
+@app.route("/", methods=['POST']) 
 def hello_world():
     return 'Hello world'
