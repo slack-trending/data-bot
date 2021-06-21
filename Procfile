2d7bbf264638065887cb92d7f1200b99b3f07367
limyeonsang <limquo@gmail.com>
2021-06-21 23:40:59 +0900

    modify

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-worker: python send_message.py
+worker: python server.py
 
 heroku config:set SLACK_BOT_TOKEN=xoxb-2215363152176-2191739510258-6GPWSiG5NONKmJZhg0FoPGn7
--- server.py
+++ server.py
@@ -1,3 +1,11 @@
-from flask import Flask, Response
-from slackeventsapi import SlackEventAdapter
-import os
+# import os
+from flask import Flask
+# from slack import WebClient
+
+app = Flask(__name__)
+
+# slack_web_client = WebClient(token = os.environ.get("SLACKBOT_TOKEN"))
+
+@app.route("/") 
+def hello_world():
+    return 'Hello world'
