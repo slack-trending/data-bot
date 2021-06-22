82cbfe79d8eb167ed246666f14b9bdc65711dceb
limyeonsang <limquo@gmail.com>
2021-06-22 12:05:39 +0900

    Initial Commit

---
+++ send_msg2.py
@@ -0,0 +1,23 @@
+import logging
+
+from flask.wrappers import Response
+logging.basicConfig(level=logging.DEBUG)
+
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+slack_token = os.environ['SLACK_BOT_TOKEN']
+client = WebClient(token=slack_token)
+
+try:
+    response = client.chat_postMessage(
+        channel="searching-trends",
+        text = "ssibal"
+    )   
+
+except SlackApiError as e:
+    assert e.response['error']
+
+    SLACK_BOT_TOKEN="xoxb-2215363152176-2191739510258-6GPWSiG5NONKmJZhg0FoPGn7" python send_msg2.py
+    
