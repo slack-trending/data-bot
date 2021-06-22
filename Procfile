13e30be6ab869912f43717f42f6427ff202cc099
limyeonsang <limquo@gmail.com>
2021-06-22 14:55:32 +0900

    Fix gunicorn call commands

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn app.py:app
+web: gunicorn --bind 0.0.0.0:5000 wsgi
 
 heroku config:set SLACK_BOT_TOKEN=xoxb-2215363152176-2191739510258-6GPWSiG5NONKmJZhg0FoPGn7
--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = Flask(__name__)
 def hello_world():
     return 'hello world!'
 
-# if __name__ == '__main__':
-#     app.run('0.0.0.0', port=5000, debug=True)
-#f
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
