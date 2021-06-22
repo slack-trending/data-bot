08fdb76267f67367bbd3f175e2b516c8a6210013
limyeonsang <limquo@gmail.com>
2021-06-22 14:23:42 +0900

    Modify

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 worker: python test.py
 
-web: gunicorn test.py:app
+web: gunicorn app.py:app
 
 heroku config:set SLACK_BOT_TOKEN=xoxb-2215363152176-2191739510258-6GPWSiG5NONKmJZhg0FoPGn7
---
+++ app.py
@@ -0,0 +1,11 @@
+from logging import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'hello world!'
+
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000, debug=True)
