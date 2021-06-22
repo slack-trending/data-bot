b55b47c1b5ed4f2f18336fe2e991981c8cc85ab6
limyeonsang <limquo@gmail.com>
2021-06-22 14:32:37 +0900

    Initial Commit

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
     return 'hello world!'
 
 # if __name__ == '__main__':
 #     app.run('0.0.0.0', port=5000, debug=True)
+#f
